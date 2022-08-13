2c47796c4bf31a05a01564ea05128a9c4bd7cecd
Isaev Mustafa <mustafaisaev3@gmail.com>
2022-08-13 10:18:29 +0300

    Add Switch prototype

--- components/Cart/Checkout/Checkout.tsx
+++ components/Cart/Checkout/Checkout.tsx
@@ -43,6 +43,8 @@ const PaymentComponent = (payment, active) => {
 
 const Checkout = ({closeCheckout}: CheckoutProps) => {
 
+  const [swithActive, setSwitchActive] = useState(false)
+
   const [paymentMethod, setPaymentMethod] = useState<PaymantMethodTypes>('CreditCard')
 
   const {values, touched, errors, handleSubmit, handleBlur, getFieldProps, handleChange} = useFormik({
@@ -99,7 +101,7 @@ const Checkout = ({closeCheckout}: CheckoutProps) => {
                 </div>
             </div>
             <div className='h-full p-[28px]'>
-                <div className='h-full w-full '>
+                <div className='h-auto w-full '>
                     <h2 className='text-[25px] text-white'>Payment Method</h2>
                     <div className='flex items-center pt-[20px] gap-2'>
                         <div className='payment active ' >
@@ -140,6 +142,29 @@ const Checkout = ({closeCheckout}: CheckoutProps) => {
                                 </div>
                             </div>
                         </div>
+                        <div className='h-auto w-full py-[30px] '>
+                            <h2 className='text-[25px] text-white'>Delivery</h2>
+                            <div className='relative'>
+                                <div className={`${swithActive ? '' : 'absolute'} w-[100%] h-[100%] z-30`  }></div>
+                                <div className= {`${swithActive ? '' : 'opacity-20'}`  }>
+                                    <div className='flex flex-col pt-3'>
+                                        <Input id='cardholder' name='cardholder' onBlur={handleBlur} label='Cardholder name' value={values.cardholder} onChange={handleChange} classes={touched.cardholder && errors.cardholder ? 'border-[1px] border-red-600' : ''} />
+                                        {/* {touched.cardholder && errors.cardholder ? <div className='text-[red]'>{errors.cardholder}</div> : null} */}
+                                    </div>
+                                    <div className='flex flex-col pt-3'>
+                                        <Input id='cardholder' name='cardholder' onBlur={handleBlur} label='Cardholder name' value={values.cardholder} onChange={handleChange} classes={touched.cardholder && errors.cardholder ? 'border-[1px] border-red-600' : ''} />
+                                        {/* {touched.cardholder && errors.cardholder ? <div className='text-[red]'>{errors.cardholder}</div> : null} */}
+                                    </div>
+                                </div>
+                            </div>
+                            <div className='pt-20px'>
+                                <div className={`w-[50px] h-[25px] rounded-[40px] bg-[#bdbdbd] flex items-center transition-all duration-300 p-[7px] cursor-pointer ${swithActive ? 'bg-[#17ff17] justify-end' : 'bg-[#bdbdbd] justify-start'}`} onClick={() => setSwitchActive(!swithActive)}>
+                                    <div className='w-[15px] h-[15px] rounded-full bg-white transition-all duration-300'></div>
+                                    {/* <motion.div className='w-[20px] h-[20px] rounded-full bg-white' animate={{x: swithActive ? 35 : 0}}></motion.div> */}
+                                    {/* <motion.div className='w-[30px] h-[30px] rounded-full bg-white' animate={{x: swithActive ? '-100%' : '100%'}}></motion.div> */}
+                                </div>
+                            </div>
+                        </div>
                         {/* <div className='flex justify-between flex-col lg:flex-row md:gap-[10px] items-center pt-[10px]'> */}
                         <div className='flex flex-col pt-10 lg:flex-row space-y-4 lg:space-y-0'>
                             <button type='submit' className='flex justify-center items-center p-[24px] cursor-pointer bg-[#EA6969] rounded-lg w-full h-[50px]'>
@@ -151,6 +176,7 @@ const Checkout = ({closeCheckout}: CheckoutProps) => {
                         </div>
                     </form>
                 </div>
+                
             </div>
         </div>
     </motion.div>
